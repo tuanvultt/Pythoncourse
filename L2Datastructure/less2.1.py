77791ba175ed387515389deefa64f48d1138c93f
tuanvu <tuavu89@gmail.com>
2022-08-11 13:23:14 +0700

    new excercise lesson 2 data structure

---
+++ L2Datastructure/less2.1.py
@@ -0,0 +1,11 @@
+string = "Python programing is the best course to learn"
+a = len(string)
+    print(a)
+b=string[0:4]
+print(b)
+c=string[-10:]
+print(c)
+change=string.find("Python")
+newstring=string[change:]
+print("{}".format("DUT"), newstring ))
+print(string.islower())
---
+++ L2Datastructure/less2.py
@@ -0,0 +1,12 @@
+A=110
+B=262
+result = A+B
+result1 = A-B
+result2 = A*B
+result3= A//B
+result4 = A%B
+print("result of {} + {} equal{}".format(A, B, result))
+print("result of {} - {} equal{}".format(A, B, result1))
+print("result of {} * {} equal{}".format(A, B, result2))
+print("result of {} // {} equal{}".format(A, B, result3))
+print("result of {} % {} equal{}".format(A, B, result4))
